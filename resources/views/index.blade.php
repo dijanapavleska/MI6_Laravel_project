b1afe2650e9c7d07f7b0f07d606245f6caab626d
Dijana <didimx.18@gmail.com>
2022-11-07 14:04:29 +0100

    importing react

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div id="future-react-app">
+        <div id="returned-app-component">
+            <h1>Welcome to MI6</h1>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/css/style.css')
+</head>
+
+<body>
+    <div class="nav-bar">
+        <img class="seal" src="/images/mi6-seal-bg.png" alt="">
+        <p>Home</p>
+        <p>People of Interest</p>
+        <div class="visibility-toggle">&lt;</div>
+    </div>
+    <div class="content">
+        @yield('content')
+
+    </div>
+
+    <script>
+        const navBar = document.querySelector(".nav-bar")
+        const navBarToggle = document.querySelector('.visibility-toggle')
+        navBarToggle.addEventListener('click', () => {
+            navBarToggle.textContent = navBarToggle.textContent == '<' ? '>' : '<'
+            navBar.classList.toggle('hidden')
+        })
+    </script>
+</body>
+
+</html>
